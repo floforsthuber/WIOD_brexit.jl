3dce228b607ade43eccd53f35fbbf9896a5c6f68
Florian Forsthuber <florian.forsthuber@gmail.com>
2021-09-29 18:39:53 +0200

    try test with value of data

--- src/WIOD_brexit.jl
+++ src/WIOD_brexit.jl
@@ -1,9 +1,8 @@
 module WIOD_brexit
 
 # Write your package code here.
-
-
-
+include("data_wrangling.jl")
+export raw_long
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,9 @@ using Test
 
 @testset "WIOD_brexit.jl" begin
     # Write your tests here.
+
+    @test round(raw_long.value[1]) == 12924.0
+
+
+
 end
